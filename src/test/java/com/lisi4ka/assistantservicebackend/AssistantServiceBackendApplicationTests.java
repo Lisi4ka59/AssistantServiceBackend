8ff81dd16c40cc2015b129d33bcf7817a8a6e5ea
Lisi4ka59 <122356446+Lisi4ka59@users.noreply.github.com>
2024-02-07 22:51:57 +0300

    Initial commit

---
+++ src/test/java/com/lisi4ka/assistantservicebackend/AssistantServiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lisi4ka.assistantservicebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistantServiceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
